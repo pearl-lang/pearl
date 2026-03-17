f1665e4fb6efaeec7dbd29c17c5c73eebd37085c
lazypwny751 <testfordnshijacking@gmail.com>
2026-03-17 16:23:17 +0300

    update argument parser

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@
 
 set -e
 
+# shellcheck disable=SC2155
 export CWD="$(pwd)"
 
 # The classic, fatal func.
@@ -14,8 +15,25 @@ die() {
 	exit 1
 }
 
+section() {
+	msg="sectioner"
+	i="0"
+	p="5"
+
+	[ -n "${1}" ] && msg="${1}"
+	[ -n "${2}" ] && p="${2}"
+
+	while [ "${i}" -lt "${p}" ] ; do
+		line="${line}="
+		i="$(( i + 1 ))"		
+	done
+
+	printf "%s %s %s\n" "${line}" "${msg}" "${line}"
+}
+
 # Check required commands.
 (
+section "Requirement check"
 must_fail=false
 for cmd in "rustup" "cargo" "rustc" ; do
 	if ! command -v "${cmd}" ; then
@@ -29,13 +47,14 @@ if "${must_fail}" ; then die "there are missing commands we require please insta
 
 # Check the base.
 (
+section "File integrity check(without checksum)"
 must_fail=false
 for ent in	"stage0:req" \
 			"stage0/src:req" \
 			"stage0/Cargo.toml:req" \
 			"stage0/src/main.rs:req" \
 			"stage0/tests:opt" ; do
-	printf "${ent%%:*}"
+	printf "%s" "${ent%%:*}"
 	if [ -e "${ent%%:*}" ] ; then
 		printf " -> exists.\n"
 	else
@@ -55,6 +74,7 @@ if "${must_fail}" ; then die "required files or directories missing, please chec
 # Stage 0
 # let's compile the bootstrap compiler.
 (
+	section "The bootstrap compiler"
 	cd "${CWD}"
 	echo "build: Stage0, bootstrap compiler.."
 	cd "stage0"
--- stage0/src/argparse.rs
+++ stage0/src/argparse.rs
@@ -8,16 +8,86 @@ pub enum Operation {
 
 pub struct Args {
 	pub operation: Operation,
+	pub base: String,
+	pub options: Option<Vec<Opt>>
+}
+
+pub enum Opts {
+	Unknown, // an unknown parameter, it gonna be error
+	Output,  // --output, -o: last parameter
+} 
+
+pub struct Opt {
+	pub opt:          Opts, // option type like '--output' is Output
+	pub long:         bool, // false is short
+	pub iterable:     bool, // when is short and iterable like -abc..
+	pub is_param_req: bool, // is any parameter required?
+	pub params:       Option<Vec<String>>
+}
+
+trait OptArg {
+	fn parse(ctx: &str, args: Vec<String>) -> Self;
+	fn identify(_par: &str, _is_long: bool) -> Opts;
+	fn is_iterable(_opt: &Opts) -> bool;
+	fn is_param_req(_opt: &Opts) -> bool;
+	fn is_long(_par: &str) -> bool;
+	fn get_params_for(_opt: &Opts, _args: Vec<String>) -> Option<Vec<String>>;
+}
+
+impl OptArg for Opt {
+	fn parse(ctx: &str, args: Vec<String>) -> Self {
+		let long: bool = Self::is_long(ctx);
+		let opt: Opts = Self::identify(ctx, long);
+		let iterable: bool = Self::is_iterable(&opt); 
+		let is_param_req: bool = Self::is_param_req(&opt);
+		let params: Option<Vec<String>> = Self::get_params_for(&opt, args);
+
+		Self {
+			opt,
+			long,
+			iterable,
+			is_param_req,
+			params
+		}
+	}
+
+	fn identify(_par: &str, _is_long: bool) -> Opts {
+		Opts::Unknown
+	}
+
+	fn is_iterable(_opt: &Opts) -> bool {
+		true
+	}
+
+	fn is_param_req(_opt: &Opts) -> bool {
+		true
+	}
+
+	fn is_long(_par: &str) -> bool {
+		true
+	}
+
+	fn get_params_for(_opt: &Opts, _args: Vec<String>) -> Option<Vec<String>> {
+		let params: Option<Vec<String>> = None;
+
+		params
+	}
 }
 
 pub fn argp(args: Vec<String>) -> Args {
 	let mut operation: Operation = Operation::Unknown;
+	let base: String = args[0].clone();
+	let options = None;
 
-	if args.len() <= 1 {
+	if args.len() > 1 {
+		
+	} else {
 		operation = Operation::None;
 	}
 
 	Args {
-		operation
+		operation,
+		base,
+		options
 	}
 }
